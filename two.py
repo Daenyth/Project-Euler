35873565b6ec4fc62d55e7ad130d1dd717742ff8
Daenyth <Daenyth+git@gmail.com>
2010-06-26 09:43:56 -0400

    two.py: Solved
    Needed even fibs only, duh

--- two.py
+++ two.py
@@ -9,8 +9,8 @@ def problem_two(limit=4000000):
     Find the sum of all the even-valued terms in the fibonacci sequence
     which do not exceed four million.
     """
-    fibs = (fib(n) for n in count())
-    return sum(takewhile(lambda x: x < limit, fibs))
+    even_fibs = (fib(n) for n in count() if fib(n) % 2 == 0)
+    return sum(takewhile(lambda x: x < limit, even_fibs))
 
 if __name__ == '__main__':
     print problem_two()
