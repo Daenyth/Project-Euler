32f88fe3a338aaa521ffdb1e8765241b4b01ea4d
Daenyth <Daenyth+git@gmail.com>
2010-06-26 11:44:28 -0400

    3: reduce max instead of max

--- three.py
+++ three.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 from daentools.primes import prime_factors
+from functools import reduce
 
 def problem_three():
     """
     Return the largest prime factor of the number 600851475143
     """
-    return max(prime_factors(600851475143))
+    return reduce(max,prime_factors(600851475143))
 
 if __name__ == '__main__':
     print problem_three()
