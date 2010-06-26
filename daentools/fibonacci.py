5f2e709e9392d062b6e5925c49b76a8e8ef038c0
Daenyth <Daenyth+git@gmail.com>
2010-06-26 09:36:54 -0400

    Add daentools module with memoize, fibonacci

---
+++ daentools/fibonacci.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from memoize import memoized
+
+@memoized
+def fib(n):
+    """
+    Return the nth fibonacci number
+    """
+    if n < 2: return n
+    return fib(n-1) + fib(n-2)
