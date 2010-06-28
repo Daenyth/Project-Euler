a79c2faa21e22862490a7069df9222fcdb7652e5
Daenyth <Daenyth+git@gmail.com>
2010-06-28 09:40:37 -0400

    6: reduce(op.add -> sum()

--- six.py
+++ six.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import operator as op
-
 def problem_six():
     """
     Find the difference between the sum of the squares of the first
@@ -17,8 +15,8 @@ def problem_six():
     ten natural numbers and the square of the sum is 3025  385 = 2640.
     """
     square = lambda x: pow(x,2)
-    sum_of_squares = reduce(op.add, map(square, xrange(1,100+1)))
-    square_of_sum = square(reduce(op.add, xrange(1,100+1)))
+    sum_of_squares = sum(map(square, xrange(1,100+1)))
+    square_of_sum = square(sum(xrange(1,100+1)))
 
     return square_of_sum - sum_of_squares
 
