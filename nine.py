a5b54bd6da695669f4d283b27efc1015ac223bad
Daenyth <Daenyth+git@gmail.com>
2010-06-30 00:39:14 -0400

    9: Solved

--- nine.py
+++ nine.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from daentools.math import is_pythagorean
+from daentools.misc import first
+
 def problem_nine():
     """
     A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
@@ -10,7 +13,11 @@ def problem_nine():
     There exists exactly one Pythagorean triplet for which a + b + c = 1000.
     Find the product abc.
     """
-    pass
+    target = 1000
+    combinations = [(a,b,target-b-a) for b in xrange(1, target/2) for a in xrange(b,target/2)]
+    triples = filter(is_pythagorean, combinations)
+    a,b,c = first(filter(lambda t: sum(t) == target, triples))
+    return a*b*c
 
 if __name__ == '__main__':
     print problem_nine()
