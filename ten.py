347157b87a31b879490ca85bd961671e46bf1b87
Daenyth <Daenyth+git@gmail.com>
2010-06-30 00:43:21 -0400

    10: Stub

---
+++ ten.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+def problem_ten():
+    """
+    Find the sum of all the primes below two million.
+    """
+    pass
+
+if __name__ == '__main__':
+    print problem_ten()
