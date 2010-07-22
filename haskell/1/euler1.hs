d608cb0b22b897bf56b07d107c3823356b74045e
Daenyth <Daenyth+git@gmail.com>
2010-07-22 11:59:29 -0400

    1.hs: Include problem definition

--- haskell/1/euler1.hs
+++ haskell/1/euler1.hs
@@ -1,5 +1,6 @@
 multOf x y = x `mod` y == 0
 
+-- Find the sum of all the multiples of 3 or 5 below 1000.
 problem1 :: (Integral a) => a -> a
 problem1 x = sum [x | x <- [1 .. (x - 1)], x `multOf` 3 || x `multOf` 5 ]
 
