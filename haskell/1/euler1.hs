4491b457c60a42fc6c1d0c0b0a73638e6662143d
Daenyth <Daenyth+git@gmail.com>
2010-07-22 00:41:00 -0400

    1.hs: More golf

--- haskell/1/euler1.hs
+++ haskell/1/euler1.hs
@@ -1,14 +1,8 @@
 module Euler1 where
 import IO
 
-wanted :: (Integral a) => a -> Bool
-wanted x
-    | x `mod` 3 == 0 = True
-    | x `mod` 5 == 0 = True
-    | otherwise = False
-
 problem1 :: (Integral a) => a -> a
-problem1 x = sum $ filter wanted [1 .. (x - 1)]
+problem1 x = sum [x | x <- [1 .. (x - 1)], x `mod` 3 == 0 || x `mod` 5 == 0 ]
 
 main = do
     let answer = show $ problem1 1000 in putStrLn answer
