eb80babb79845c774288eda525155681c2e4eb00
Daenyth <Daenyth+git@gmail.com>
2010-07-22 12:00:34 -0400

    1.hs: Make variable names less ambiguous

--- haskell/1/euler1.hs
+++ haskell/1/euler1.hs
@@ -2,7 +2,7 @@ multOf x y = x `mod` y == 0
 
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 problem1 :: (Integral a) => a -> a
-problem1 x = sum [x | x <- [1 .. (x - 1)], x `multOf` 3 || x `multOf` 5 ]
+problem1 lim = sum [x | x <- [1 .. (lim - 1)], x `multOf` 3 || x `multOf` 5 ]
 
 main :: IO ()
 main = print $ problem1 1000
