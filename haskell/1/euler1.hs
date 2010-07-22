5f8b02c4ecbd939c4d74709aba37fbb07cecd6c0
Daenyth <Daenyth+git@gmail.com>
2010-07-22 00:57:26 -0400

    1.hs: add multOf() to make more readable

--- haskell/1/euler1.hs
+++ haskell/1/euler1.hs
@@ -1,8 +1,10 @@
 module Euler1 where
 import IO
 
+multOf x y = x `mod` y == 0
+
 problem1 :: (Integral a) => a -> a
-problem1 x = sum [x | x <- [1 .. (x - 1)], x `mod` 3 == 0 || x `mod` 5 == 0 ]
+problem1 x = sum [x | x <- [1 .. (x - 1)], x `multOf` 3 || x `multOf` 5 ]
 
 main = do
     let answer = show $ problem1 1000 in putStrLn answer
