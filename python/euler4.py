b2de222649c988ce9537080eeb754f4af500424d
Daenyth <Daenyth+git@gmail.com>
2010-07-23 02:08:44 -0400

    4.py: de-stupid

--- python/euler4.py
+++ python/euler4.py
@@ -13,20 +13,8 @@ def problem_four():
     The largest palindrome made from the product of
     two 2-digit numbers is 9009 = 91 * 99.
     """
-    palindrome = None
-    numbers = ["".join(combo) for combo in product([str(x) for x in range(10)], repeat=3)]
-    combos = combinations(numbers, 2)
-
-    for x,y in combos:
-        x = int(x)
-        y = int(y)
-        if x is 0 or y is 0: continue
-        xy = x * y
-        if is_palindrome(xy):
-            if xy > palindrome:
-                palindrome = xy
-
-    return palindrome
+    numbers = [x*y for x in xrange(100,999+1) for y in xrange(100,999+1)]
+    return max(filter(is_palindrome, numbers))
 
 if __name__ == '__main__':
     print problem_four()
