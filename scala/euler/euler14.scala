42fad9b9527ba909826f79bde1d744c47ee8c4f7
Daenyth <Daenyth+git@gmail.com>
2014-04-26 21:59:42 -0400

    Euler14 solution in scala

---
+++ scala/euler/euler14.scala
@@ -0,0 +1,46 @@
+package euler
+import scala.collection.mutable
+import Stream.cons
+
+object Euler14 {
+  val cache = mutable.Map[Long, Long]()
+
+  def nextCollatz(n: Long): Long ={
+    n % 2 match {
+      case 0 => n / 2
+      case 1 => 3 * n + 1
+    }
+  }
+
+  def memoCollatz(n: Long): Long =
+    cache.get(n) match {
+      case Some(next) => next
+      case None =>
+        val next = nextCollatz(n)
+        cache += (n -> next)
+        next
+    }
+
+  def collatzChain(start: Long): Stream.Cons[Long] =
+    cons(
+      start,
+      {
+        val next = memoCollatz(start)
+        if(next == 1)
+          Stream.empty
+        else
+          cons(next, collatzChain(next))
+      }
+    )
+
+  def chainLength(n: Long) = collatzChain(n).length
+
+  def main(args: Array[String]) {
+    val max = (1 to 1000000).map { n =>
+      print("\r" + n)
+      n -> chainLength(n)
+    }.maxBy(_._2)
+    println()
+    println(max)
+  }
+}
